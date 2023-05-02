201c3426eabe87e72e4bbee3c28582119adf8c5a
ajyshrma <ajyshrma69@gmail.com>
2023-05-02 10:43:11 +0530

    Added function to generate access token from refresh token

--- src/ZohoClient.php
+++ src/ZohoClient.php
@@ -3,7 +3,6 @@
 namespace ajyshrma69\ZohoConnector;
 
 use Exception;
-use zsign\ZohoException;
 
 class ZohoClient extends Config
 {
@@ -42,6 +41,38 @@ class ZohoClient extends Config
     {
         try {
             $url = $this->getApiUrl() . "token?code=$this->code&client_id=" . $this->client_id . "&client_secret=" . $this->client_secret . "&redirect_uri=" . $this->redirect_url . "&grant_type=authorization_code";
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+            ));
+            $response   = curl_exec($curl);
+            curl_close($curl);
+            $response = json_decode($response);
+
+            return $response;
+            if (!property_exists($response, 'error')) {
+                if (property_exists($response, 'access_token')) {
+                    $this->setAccessToken($response->access_token);
+                }
+                if (property_exists($response, 'refresh_token')) {
+                    $this->setRefreshToken($response->refresh_token);
+                }
+                return $this;
+            }
+        } catch (Exception $e) {
+            throw new ZohoException($e->getMessage(), 400);
+        }
+    }
+
+
+    public function generateAccessTokenUsingRefreshToken()
+    {
+        try {
+            $url = $this->getApiUrl() . "token?client_id=" . $this->client_id . "&client_secret=" . $this->client_secret . "&refresh_token=" . $this->refresh_token . "&grant_type=refresh_token";
+
             $curl = curl_init();
             curl_setopt_array($curl, array(
                 CURLOPT_URL => $url,
