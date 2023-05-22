33597cbef473e56558f46ac4ab3c0e5f44a05dac
ajyshrma <ajyshrma69@gmail.com>
2023-05-22 10:11:55 +0530

    update zoho exception for exceptionionion return

--- src/ZohoRequest.php
+++ src/ZohoRequest.php
@@ -22,13 +22,14 @@ class ZohoRequest
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-
+            $headers = $this->getRequestHeaders();
             curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
             $result = curl_exec($ch);
             curl_close($ch);
+
             return $result;
-        } catch (Exception $e) {
-            throw new Exception($e->getMessage());
+        } catch (ZohoException $e) {
+            throw new ZohoException($e->getMessage());
         }
     }
 
